1159af334cf99c0ba09d6b0aa017d19625796d53
Bastian Müller <McBeath@gmx.net>
2010-08-31 17:51:35 +0200

    refactoring and some cleanup done

---
+++ src/com/nogy/afu/soundmodem/jsoundmodem.java
@@ -0,0 +1,28 @@
+/*
+ *  Simple demo application sending testdata
+ */
+package com.nogy.afu.soundmodem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class jsoundmodem extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void do_TX(View v)
+    {
+    	Message msg;
+    	APRSFrame frame = new APRSFrame("NOCALL", "APA110", "WIDE1-1", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/*-+%&etc...", 20);
+    	msg = frame.getMessage();
+//    	for (i=0; i<msg.numberOfBits; i++)
+//    		Log.d("msg", "["+Integer.toString(i/8)+"]="+Integer.toString((msg.data[i/8] & (1<<i%8))>>i%8));
+    	Afsk a = new Afsk();
+    	a.sendMessage(msg);
+    }
+}
